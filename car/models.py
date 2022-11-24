1d068776f9811dd539a51fb1d81c66ee67260ff1
ran <ran@goz.com>
2022-11-24 10:30:50 +0200

    treatment and employee  models added

--- car/models.py
+++ car/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Car(models.Model):
     class CarType(models.TextChoices):
@@ -9,4 +10,21 @@ class Car(models.Model):
     cartype = models.CharField(max_length=3, choices=CarType.choices, default=CarType.CAR)
     number = models.CharField(max_length=20, null=False)
     manufacturer = models.CharField(max_length=20)
+
+
+class Employee(User):
+    tagnumber = models.CharField(max_length=50)
+
+class Treatment(models.Model):
+    class TreatmentType(models.IntegerChoices):
+        TEN_TOUSAND = 1, '10,000'
+        BREAKS = 2, 'BREAKS'
+        THIRTY_THOUSAND = 3, '30,000'
+        
+    type = models.IntegerField(choices=TreatmentType.choices, null=False)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+
+
+
 
