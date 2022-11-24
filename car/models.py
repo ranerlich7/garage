7e0586e9fdce3c17e4cfde7285ea0916d314340e
ran <ran@goz.com>
2022-11-24 11:01:12 +0200

    added template - bootstrap + images

--- car/models.py
+++ car/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 class Car(models.Model):
     class CarType(models.TextChoices):
-        CAR = 'C', 'CAR'
-        TRUCK = 'T', 'TRUCK'
-        MOTORCYCLE = 'M', 'MOTORCYCLE'
+        CAR = 'C', 'Car'
+        TRUCK = 'T', 'Truck'
+        MOTORCYCLE = 'M', 'Motorcycle'
 
     cartype = models.CharField(max_length=3, choices=CarType.choices, default=CarType.CAR)
     number = models.CharField(max_length=20, null=False)
     manufacturer = models.CharField(max_length=20)
+    image = models.ImageField(null=True, blank=True,
+                              default='car.png')
 
