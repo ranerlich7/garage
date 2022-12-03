e50b926bc058e31da487e2fc8bdcd948a0c55cf0
ran <ran@goz.com>
2022-12-03 10:34:33 +0200

    added back cars list view

--- car/urls.py
+++ car/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name="car"
 urlpatterns = [
+    path('', views.cars, name="cars"),
     path('t', views.treatment, name="edit_treatment"),
     path('list', views.treatments, name="tlist"),
     path('charts', views.charts, name="charts")
